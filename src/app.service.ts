70dce54c8a0df9e37d60e451bba13c060803c974
mk-elbaz <66201789+mk-elbaz@users.noreply.github.com>
2022-05-12 14:47:58 +0200

    kafka try

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { ProducerService } from './kafka/producer.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly producerService: ProducerService) { }
+
+  async getHello() {
+    await this.producerService.produce({
+      topic: 'test',
+      messages: [
+        {
+          value: 'Hello BAZ',
+        },
+      ],
+    });
+    return 'It worked!';
   }
+
+  async getHello2() {
+    await this.producerService.produce({
+      topic: 'ble3',
+      messages: [
+        {
+          value: 'Hello abdo',
+        },
+      ],
+    });
+    return 'It worked!';
+  }
+
+  
 }
